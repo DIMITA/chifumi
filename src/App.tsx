4dc79c5478954409d94cd4c9434e7c182ba817fc
DIMITA <dimitriomorales01@gmail.com>
2023-02-13 12:59:28 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='flex'>
+        <div className='w-full'></div>
+        <div className='w-full'></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
